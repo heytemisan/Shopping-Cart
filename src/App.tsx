fdb2d81f455c2ecd8d502408d848bd12262a5b5a
Heytemisan <temyicy@gmail.com>
2022-01-28 19:56:26 +0100

    feat: created App.styles component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
-const App =()=> {
+import { useState } from "react";
+import { useQuery } from "react-query";
+//components
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+//styles
+import {Wrapper} from './App.sytles'
+
+
+const App = () => {
   return (
     <div className="App">
         hello
